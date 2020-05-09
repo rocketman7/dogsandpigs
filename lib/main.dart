738278b9a2db6b0025bf0e22876ba5ea2a8a3b36
csejun <csejun@naver.com>
2020-05-09 14:19:53 +0900

    기본 뼈대까지 우선 올림 screen_logIn -> jk screen_home -> csejun 수정 고고

--- lib/main.dart
+++ lib/main.dart
@@ -1,111 +1,42 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+import 'screens/screen_home.dart';
+import 'screens/screen_logIn.dart';
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+
+//앱의 기초가 되는 부분. 아래 MainApp 은 혹시 앱 전체를 아우르는 provider 변수가 필요할 경우나(추측),
+//전체 테마 등을 변경할 때만 수정하면 될 듯
+//바로 MainRoute 를 호출하여 로그인 or 홈화면으로 넘어간다.
+void main() => runApp(MainApp());
+
+class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        brightness: Brightness.light,
+        primaryColor: Colors.white,
+        accentColor: Colors.black,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MainRoute(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+//앱이 실행되면 먼저 메인루트인 당 페이지를 호출한다.
+//로그인이 되지 않았다면 login screen 을 호출하고,
+//로그인이 되었다면 앱의 메인화면인 home screen 을 호출한다(아마도 log in할 때의 data 를 가지고?). 그 후의 모든 화면은 home screen 으로부터 파생되면 될듯???
+class MainRoute extends StatelessWidget {
+  //나중에 이 부분과 밑 build 안쪽 부분을 실제 로그인 여부를 가릴 수 있는 코드로 수정해준다.
+  final bool isLogInSuccess = false;
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+    if (isLogInSuccess) {
+      return HomeScreen();
+    } else {
+      return LogInScreen();
+    }
   }
 }
---
+++ lib/screens/screen_logIn.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+
+class LogInScreen extends StatefulWidget {
+  @override
+  _LogInScreenState createState() => _LogInScreenState();
+}
+
+class _LogInScreenState extends State<LogInScreen> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('로그인 페이지 구현 필요'),
+      ),
+    );
+  }
+}
