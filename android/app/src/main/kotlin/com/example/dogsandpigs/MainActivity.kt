b1bcb83b9d3c8f38b8a5b5c1007495b98a582faa
rocketman <ryomajk@gmail.com>
2020-05-09 14:06:21 +0900

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/dogsandpigs/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.dogsandpigs
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
